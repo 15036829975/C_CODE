1192ced8b1883defb204ed6ce3e96bec25df2a58
15036829975 <wang15036829975@163.com>
2023-05-19 19:27:03 +0800

    学习字符串以及转义字符

---
+++ Project_05_19/Project_05_19/源.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	//char ch='w'
+//	char arr1[]="abcdef";//\0是字符串的结束
+//	char arr2[] = {'a','b','c'};
+//	/*printf("%s\n", arr2);*/
+//	//int len = strlen(arr1);//求字符串长度
+//	//int len = strlen(arr2);
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//
+//	return 0;
+//}
+//int main()
+//{
+//	printf("\n\0deq");//转义字符
+//
+//	return 0;
+//}
+int main()
+{
+	printf("%c\n", '\'');
+	return 0;
+}
