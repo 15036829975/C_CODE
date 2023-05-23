94589d9f5d32e6588191ce52dbee3c4a168951d1
15036829975 <wang15036829975@163.com>
2023-05-23 16:39:24 +0800

    学习函数，数组，在牛客网完成几道编码题

---
+++ Project_05_23/Project_05_23/源.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//数组
+//int main()
+//{
+//	int arr[] = { 0,1,2,31,4,5,6 };
+//	/*printf("%d\n", arr[3]);*/
+//	int i = 0;
+//	while (i < 7)
+//	{
+//		printf("%d\n", arr[i]);
+//		i = i + 1;
+//	}
+//	return 0;
+//}
+//int num = 10;
+//int main()
+//{
+//	int num = 1;
+//	printf("num=%d\n", num);
+//	return 0;
+//}
+//作业
+int main()
+{
+	printf("Name    Age    Gender\n");
+	printf("---------------------\n");
+	printf("Jack    18    man\n");
+
+	return 0;
+}
