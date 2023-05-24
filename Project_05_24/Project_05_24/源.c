17cab77095e90d41935bae4c468f2d0178540151
15036829975 <wang15036829975@163.com>
2023-05-24 15:12:02 +0800

    学习操作符

---
+++ Project_05_24/Project_05_24/源.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	//int a = 7 / 2;
+	//int b = 7 % 2;//取模
+	//printf("%d\n", a);
+	//printf("%d\n", b);
+	//float a = 7 / 2.0;
+	//int b = 7 % 2;//取模，且两端只能为整数
+	//printf("%.1f\n", a);//加点就是几位
+	//printf("%d\n", b);
+	//int a = 0;
+	//a = 10;//赋值
+	//a = a + 3;
+	int a = 0;
+	if (!a)
+	{
+
+		printf("as\n", a);
+	}
+	return 0;
+}
