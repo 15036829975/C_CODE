6d42b7a0434fd59f9dae39773398b42862b0f74a
15036829975 <wang15036829975@163.com>
2023-05-25 15:37:15 +0800

    操作符，以及关键字

---
+++ Project_05_25/Project_05_25/源.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//操作符
+
+int main()
+{
+	/*float a = 7 / 2.0;
+	printf("%.1f\n",a);
+
+	return 0;*/
+	//&& 逻辑与
+    //||逻辑或
+	/*int a = 0; 
+		int b = 2;
+		if (a || b)
+		{
+			printf("sad\n");
+			return 0;
+		}*/
+	//三目操作符
+	/*int a = 330;
+	int b = 20;
+	int r = (a > b ? a : b);
+	printf("%d\n", r);
+	return 0;*/
+	int arr[10] = { 1,2,3,4,5,6,7,8,9 };
+	arr[3] = 20;//[]是下标引用操作符
+	return 0;
+}
