197b6f9f276b75adf54c07b65530818458a8c8e3
15036829975 <wang15036829975@163.com>
2023-06-05 20:43:29 +0800

    学习循环和分支语句

---
+++ Project_06_05/Project_06_05/源.c
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int test()
+//{
+//	int a = 4;
+//     if (a == 3)
+//         return 1;
+//     else
+//     return 0;
+//}
+//int main()
+////{
+////	char s[] = { 1,2,3,4 };
+////	if (s[3] <= 5)
+////	
+////
+////		printf("as");
+//{
+//    int r = test();
+//    printf("%d\n", r);
+//    return 0;
+//	}
+int main()
+{
+	/*int num = 0;
+	scanf("%d", &num);
+	if (num % 2 == 1)
+		printf("ÆæÊý\n");
+	else
+		printf("no\n");*/
+	////int num = 0;
+	////while (num <= 100)
+	////{
+	////	if (num % 2 == 1)
+	////	{
+	////		printf("%d ", num);
+
+
+	////	}
+	////	num++;
+	////}
+	////
+
+
+
+	//int day = 1;
+	//  switch (day)
+	//{
+	//case 1:
+	//	printf("ÐÇÆÚ1\n");
+	//	break;
+	//case 2:
+	//	printf("ÐÇÆÚ2\n");
+	//	break;
+	//}
+
+	int day = 1;
+	switch (day)
+	{
+	case 1:
+	case 2:
+		printf("11");
+		break;
+		default:
+			printf("sdfh");
+			break;
+	}
+	return 0;
+}
