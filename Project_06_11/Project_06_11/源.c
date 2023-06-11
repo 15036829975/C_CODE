06105dccc9c9a8fbcd74953e00f369937b3d77b7
15036829975 <wang15036829975@163.com>
2023-06-11 19:17:03 +0800

    学习FOR语句

---
+++ Project_06_11/Project_06_11/源.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	/*int i = 6;
+	for (i=1;i<=10;i++)
+		{
+		if (5 == i)
+			break;
+		printf("%d ", i);
+
+		}*/
+	//int i = 0;
+	//int j = 0;
+	//for (i = 0; i <= 3; i++)
+	//{
+	//	for (j = 0; j <= 3; j++)
+	//	{
+	//		printf("asd\n");
+	//	}
+	//}
+	int i = 0;
+	int k = 0;
+	for (i = 0, k = 0; k == 0; i++, k++)
+		k++;
+	return 0;
+}
