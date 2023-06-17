19085823f0d88b800ee93025ad8f3f25d36bc8e7
15036829975 <wang15036829975@163.com>
2023-06-17 20:12:06 +0800

    学习do while语句

---
+++ Project_06_17/Project_06_17/源.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int a = 1;
+//	do
+//	{
+//		if (a == 5)
+//			continue;
+//		printf("%d", a);
+//		a++;
+//	}
+//	while (a <= 10);
+//	return 0;
+//}
+//int main()
+//{
+//	/*int i = 1;
+//	int n = 0;
+//	int s = 1;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		s = s * i;
+//	}
+//	printf("%d", s);*/
+//	/*int main()*/
+////	int i = 0;
+////	int n = 1;
+////	int s = 1;
+////	int q = 0;
+////	/*for (n = 1; n <= 10; n++)
+////	{
+////		int s = 1;
+////		for (i = 1; i <= n; i++)
+////		{
+////			s = s * i;
+////		}
+////		q = q + s;
+////	}*/
+////	for (i = 1; i <= 3; i++)
+////	{
+////		n = n * i;
+////		q = q + n;
+////
+////	}
+////	printf("%d", q);
+////	return 0;
+////}
+//int main()
+//{
+//	int a[10] = { 1,2,3,4,5,6,7,8,9 };
+//
+//	return 0;
+//}
