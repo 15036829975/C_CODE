7246c4c909270435f82b822675fd9f5cf7af0b18
15036829975 <wang15036829975@163.com>
2023-07-11 19:21:07 +0800

    递归和非递归，迭代

---
+++ Project_07_11/Project_07_11/源.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int fib(int n)
+//{
+//	if (n <= 2)
+//		return 1;
+//	else
+//		return fib(n - 1) + fib(n - 2);
+//}
+int fib(int n)
+{
+	int a = 1;
+	int b = 1;
+	int c = 1;
+	while (n >= 3)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		n--;
+	}
+	return c;
+}
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	int ret = fib(n);
+	printf("%d", ret);
+	return 0;
+}
+
