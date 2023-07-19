3ea3bb4ede6a4b8ceca4b3b88b8c71bfad20b0c7
15036829975 <wang15036829975@163.com>
2023-07-19 14:34:11 +0800

    二维数组的学习

---
+++ Project_07_19/Project_07_19/源.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int arr1[3][4] = {1,3,5,0,1,2,4,4,5,6,8,9};
+	char arr2[3][4] = { {1,3},{2,3,3},{3,5,6,8} };
+
+
+
+	return 0;
+}
