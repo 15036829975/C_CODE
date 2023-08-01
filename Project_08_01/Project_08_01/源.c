8a5d6364a0072e4816ea37016e01c5dcd397c335
15036829975 <wang15036829975@163.com>
2023-08-01 20:18:04 +0800

    表达式的计算

---
+++ Project_08_01/Project_08_01/源.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	/*int a = 2+ 6 / 3;
+	int b = 2 + 3 + 4;*/
+	char a = 5;
+	char b = 125;
+	char c = a + b;
+	printf("%d\n", c);
+	/*char c = -1;*/
+
+
+
+
+
+	return 0;
+}
