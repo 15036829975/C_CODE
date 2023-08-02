ca1aed7e7506725743f0d8133f60b6afed5dc952
15036829975 <wang15036829975@163.com>
2023-08-02 20:34:19 +0800

    指针类型，指针变量

---
+++ Project_08_02/Project_08_02/源.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	/*int a = 0;
+	int* p = &a;*/
+
+	char* pc = NULL;
+	int* ps = NULL;
+	printf("%zu\n", sizeof(pc));
+	printf("%zu", sizeof(ps));
+	return 0;
+}
